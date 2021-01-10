0f901cf4469be18fe693630b6ffbffcb21f070ed
shanePark <kkobuk@hotmail.co.kr>
2021-01-10 19:28:50 +0900

    innitial lv2 test passed

---
+++ programmers/src/kv2/StockPrice.java
@@ -0,0 +1,26 @@
+package kv2;
+
+import java.util.Arrays;
+
+public class StockPrice {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution((new int[]{1, 2, 3, 2, 3}))));
+
+	}
+	
+    static public int[] solution(int[] prices) {
+    	final int SIZE = prices.length;
+        int[] answer = new int[SIZE];
+        for(int i=0; i<SIZE; i++ ) {
+        	for(int j=i+1; j<SIZE; j++) {
+        		answer[i]++;
+        		if(prices[i] > prices[j])
+        			break;
+        	}
+        		
+        }
+        return answer;
+    }
+
+}
