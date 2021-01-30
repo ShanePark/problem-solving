071ec7667c487a30ce4b590ba6e96d178dd804eb
shanePark <kkobuk@hotmail.co.kr>
2021-01-30 22:55:55 +0900

    3 points!

--- src/lv2/N_SkillTree.java
+++ src/lv2/N_SkillTree.java
@@ -3,9 +3,9 @@ package lv2;
 public class N_SkillTree {
 
 	public static void main(String[] args) {
-		System.out.println(solution("CBD",new String[]{"BACDE", "CBADF", "AECB", "BDA"}));
-		System.out.println(solution("CBD",new String[]{"CAD"}) == 0);
-		System.out.println(solution("REA",new String[]{"POA"}) == 0 );
+//		System.out.println(solution("CBD",new String[]{"BACDE", "CBADF", "AECB", "BDA"}));
+		System.out.println(solution("CBD",new String[]{"AECB"}));
+//		System.out.println(solution("REA",new String[]{"POA"}) == 0 );
 	}
 	
     static public int solution(String skill, String[] skill_trees) {
@@ -18,12 +18,14 @@ public class N_SkillTree {
     static int isPossible(String skill, String tree) {
     	for(int i=0; i<skill.length(); i++) {
     		for(int j= i-1 ; j>=0; j--) {
-    			if(tree.indexOf(skill.charAt(i)) != -1 && tree.indexOf(skill.charAt(j)) == -1)
+    			if(tree.indexOf(skill.charAt(i)) != -1 && tree.indexOf(skill.charAt(j)) == -1) {
     				return 0;
+    			}
     		}
     		for(int j=i+1; j<skill.length(); j++) {
-    			if(tree.indexOf(skill.charAt(i)) > tree.indexOf(skill.charAt(j)))
+    			if(tree.indexOf(skill.charAt(j))!=-1 && (tree.indexOf(skill.charAt(i)) > tree.indexOf(skill.charAt(j)))) {
     				return 0;
+    			}
     		}
     	}
     	return 1;
