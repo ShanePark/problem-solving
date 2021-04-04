978861ed5c648ae5d93f53104272942cb35104e0
ShanePark <psh40963@naver.com>
2021-04-04 14:54:56 +0900

    it's been ages since my last programmers. BOOM!
    one point..? but I admit

---
+++ src/lv2/O_minAndMaxValue.java
@@ -0,0 +1,27 @@
+package lv2;
+
+import java.util.Arrays;
+
+public class O_minAndMaxValue {
+
+	public static void main(String[] args) {
+		System.out.println(solution("1 2 3 4"));
+	}
+	
+    public static String solution(String s) {
+    	String[] arr = s.split(" ");
+    	System.out.println(Arrays.toString(arr));
+    	int min = Integer.parseInt(arr[0]);
+    	int max = min;
+    	
+    	for(String str : arr) {
+    		int number = Integer.parseInt(str);
+    		min = min > number ? number : min;
+    		max = max < number ? number : max;
+    	}
+    	
+        String answer = min + " " + max;
+        return answer;
+    }
+
+}
