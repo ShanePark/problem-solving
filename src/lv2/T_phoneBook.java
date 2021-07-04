0a0f62cb6a6d28217f77482a1f6926a4705c9fa4
Shane Park <psh40963@naver.com>
2021-07-04 10:56:41 +0900

    phonebook +5 point

---
+++ src/lv2/T_phoneBook.java
@@ -0,0 +1,27 @@
+package lv2;
+
+/**
+ *accuracy : passed
+ *efficiency : failed
+ *
+ */
+public class T_phoneBook {
+	
+	public static void main(String[] args) {
+		String[] arr = {"12","123","1235","567","88"};
+		System.out.println(solution(arr));
+	}
+	
+    public static boolean solution(String[] phone_book) {
+    	for(String num : phone_book) {
+    		for(String nums : phone_book) {
+    			if(nums.startsWith(num)) {
+    				if(!nums.equals(num))
+    					return false;
+    			}
+    		}
+    	}
+        return true;
+    }
+
+}
