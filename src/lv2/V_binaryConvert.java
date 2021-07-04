f48d7913a02b08451b108c633f8707e57433e2c4
Shane Park <psh40963@naver.com>
2021-07-04 21:00:36 +0900

    binaryConvert Passed . + 1 point

---
+++ src/lv2/V_binaryConvert.java
@@ -0,0 +1,57 @@
+package lv2;
+
+import java.util.Arrays;
+
+public class V_binaryConvert {
+	
+	public static void main(String[] args) {
+		int[] arr = solution("110010101001");
+		System.out.println(Arrays.toString(arr));
+	}
+	
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+        
+        int convertCount = 0;
+        WrapInt zeroCount = new WrapInt();
+        
+        while(true) {
+        	if("1".equals(s)) break;
+        	s = binaryConvert(s, zeroCount);
+        	convertCount++;
+        }
+        
+        answer[0] = convertCount;
+        answer[1] = zeroCount.value;
+        
+        return answer;
+    }
+    
+    public static String binaryConvert(String s, WrapInt zeroCount) {
+    	int length = s.length();
+    	StringBuffer sb = new StringBuffer(s);
+    	int zeros = 0;
+    	for(int i=0; i<length; i++) {
+    		if(sb.charAt(i-zeros) == '0') {
+    			sb.deleteCharAt(i-zeros);
+    			zeros ++;
+    		}
+    	}
+    	
+    	length = sb.length();
+    	zeroCount.value += zeros;
+    	return Integer.toBinaryString(length);
+    	
+    }
+
+}
+
+class WrapInt{
+	int value;
+}
+
+
+
+
+
+
