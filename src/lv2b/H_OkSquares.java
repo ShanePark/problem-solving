cca963ed834239b5dd4a991f0f6425b253e443b0
Shane Park <psh40963@naver.com>
2021-07-14 10:54:59 +0900

    variable name changed

--- src/lv2b/H_OkSquares.java
+++ src/lv2b/H_OkSquares.java
@@ -4,18 +4,17 @@ public class H_OkSquares {
 
 	public static void main(String[] args) {
 		System.out.println(solution(8,12));
-
 	}
 	
     public static long solution(int w, int h) {
-    	int largestCommonMultiple = 0;
+    	int	GreatestCommonDivisor = 0;
     	int max = w < h ? h : w;
     	for(int i=1; i<=max; i++) {
     		if(w%i ==0 && h%i ==0) {
-    			largestCommonMultiple = i;
+    			GreatestCommonDivisor = i;
     		}
     	}
-        long answer = (long)w * (long)h - ((long)w + (long)h -largestCommonMultiple);
+        long answer = (long)w * (long)h - ((long)w + (long)h -GreatestCommonDivisor);
         return answer;
     }
 
