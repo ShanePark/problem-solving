51244425153fa2793fa19b8563005f4f616ce2eb
Shane Park <psh40963@naver.com>
2021-07-20 17:55:13 +0900

    깊이/너비 우선 탐색(DFS/BFS) 네트워크 only +2 points ?!

---
+++ src/main/java/shane/programmers/lv2b/N_Network.java
@@ -0,0 +1,101 @@
+package shane.programmers.lv2b;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+채점을 시작합니다.
+정확성  테스트
+테스트 1 〉	통과 (0.39ms, 52.3MB)
+테스트 2 〉	통과 (0.29ms, 53MB)
+테스트 3 〉	실패 (메모리 초과)
+테스트 4 〉	실패 (메모리 초과)
+테스트 5 〉	통과 (0.28ms, 52.6MB)
+테스트 6 〉	실패 (메모리 초과)
+테스트 7 〉	통과 (0.62ms, 56.1MB)
+테스트 8 〉	실패 (메모리 초과)
+테스트 9 〉	실패 (메모리 초과)
+테스트 10 〉	실패 (메모리 초과)
+테스트 11 〉	실패 (메모리 초과)
+테스트 12 〉	실패 (메모리 초과)
+테스트 13 〉	실패 (메모리 초과)
+채점 결과
+정확성: 30.8
+합계: 30.8 / 100.0
+ *
+ */
+public class N_Network {
+	
+	public static void main(String[] args) {
+		System.out.println(solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+		System.out.println(solution(3, new int[][] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
+		System.out.println(solution(6, new int[][] {{1, 0, 1, 1, 0, 0}, {0, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 1, 1}, {1, 0, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1}}));
+	}	
+	
+    public static int solution(int n, int[][] computers) {
+    	
+        Map<Integer, Network> networkMap = new HashMap<>();
+
+        for(int i=0; i<n; i++) {
+        	Network network = networkMap.get(i);
+        	if(network == null) {
+        		network = new Network(i);
+        	}
+        	networkMap.put(i, network);
+        	
+        	for(int j=0; j<n; j++) {
+        		if(computers[i][j] == 1 && i!=j) {
+        			Network connectedNetwork = networkMap.get(j);
+        			if(connectedNetwork == null) {
+        				networkMap.put(j, network);
+        				network.computers.add(j);
+        			}else {
+        				// network를 모두 connectedNetwork로 이관
+        				int[] oldList = new int[network.computers.size()];
+        				for(int k=0; k<network.computers.size(); k++) {
+        					oldList[k] = network.computers.get(k);
+        				}
+        				for(int key : oldList) {
+        					networkMap.put(key, connectedNetwork);
+        					connectedNetwork.computers.add(key);
+        				}
+        				network = connectedNetwork;
+        			}
+        		}
+        	}
+        }
+        
+        Set<Network> networks = new HashSet<>();
+        for(int i=0; i<n; i++) {
+        	networks.add(networkMap.get(i));
+        }
+        
+        return networks.size();
+    }
+	
+  
+
+}
+
+class Network{
+	int networkNumber;
+	List<Integer> computers;
+	
+	Network(int networkNumber){
+		this.networkNumber = networkNumber;
+		computers = new ArrayList<>();
+		computers.add(networkNumber);
+	}
+
+	@Override
+	public String toString() {
+		return "Network [networkNumber=" + networkNumber + ", computers=" + computers + "]";
+	}
+	
+	
+}
+
---
+++ src/main/java/shane/programmers/lv2b/N_Network2.java
@@ -0,0 +1,61 @@
+package shane.programmers.lv2b;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class N_Network2 {
+	
+	public static void main(String[] args) {
+		System.out.println(solution(3, new int[][] {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
+		System.out.println(solution(3, new int[][] {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
+		System.out.println(solution(6, new int[][] {{1, 0, 1, 1, 0, 0}, {0, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 1, 1}, {1, 0, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1}}));
+	}	
+	
+    public static int solution(int n, int[][] computers) {
+    	
+        Map<Integer, Integer> networkMap = new HashMap<>();
+
+        for(int i=0; i<n; i++) {
+        	Integer network = networkMap.get(i);
+        	if(network == null) {
+        		network = i;
+        	}
+        	networkMap.put(i, network);
+        	
+        	for(int j=0; j<n; j++) {
+        		if(computers[i][j] == 1 && i!=j) {
+        			Integer connectedNetwork = networkMap.get(j);
+        			if(connectedNetwork == null) {
+        				networkMap.put(j, network);
+        			}else {
+        				// 기존 network 이관
+        				move(networkMap, network, connectedNetwork);
+        				network = connectedNetwork;
+        			}
+        		}
+        	}
+        }
+        
+        Set<Integer> networks = new HashSet<>();
+        for(int i=0; i<n; i++) {
+        	networks.add(networkMap.get(i));
+        }
+        
+        return networks.size();
+    }
+    
+    static void move(Map<Integer,Integer> networkMap, Integer from, Integer to) {
+    	for(Integer key : networkMap.keySet()) {
+    		if(networkMap.get(key) == from) {
+    			networkMap.put(key, to);
+    		}
+    	}
+    }
+	
+  
+
+}
+
+
