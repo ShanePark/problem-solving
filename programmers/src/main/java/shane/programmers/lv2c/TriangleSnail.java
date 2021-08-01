2ce98985b43fb29445afd4f7832c337c50ed8168
Shane Park <psh40963@naver.com>
2021-08-01 13:02:00 +0900

    월간 코드 챌린지 시즌 1 > 삼각 달팽이 + 1 point

---
+++ programmers/src/main/java/shane/programmers/lv2c/TriangleSnail.java
@@ -0,0 +1,58 @@
+package shane.programmers.lv2c;
+
+import java.util.Arrays;
+
+public class TriangleSnail {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(solution(4)));
+	}
+	
+    public static int[] solution(int n) {
+    	
+    	int[][] array = new int[n][];
+    	for(int i=0; i<n; i++) {
+    		int[] arr = new int[i+1];
+    		array[i] = arr;
+    	}
+    	
+    	int summaryUntilN = summaryUntilN(n);
+    	
+    	int y=0, x=0;
+    	for(int i=1; i<=summaryUntilN; i++) {
+//    		System.out.println("y : " + y + ", x : " + x);
+    		array[y][x] = i;
+    		
+    		if(
+    			x!=0 && (
+    					(x == array[y].length-1 && array[y-1][x-1] == 0)
+    					|| ( x<array[y].length-1 && (array[y][x+1] != 0) && array[y-1][x-1] == 0)
+    			)
+    		) {
+    			x--;
+    			y--;
+    		}else if(y==array.length-1 || array[y+1][x] !=0){
+    			x++;
+    		}else {
+    			y++;
+    		}
+    	}
+    	
+//    	System.out.println(Arrays.deepToString(array));
+    	
+    	int[] answer = new int[summaryUntilN];
+    	int i = 0;
+    	for(int[] arr : array) {
+    		for(int num : arr) {
+    			answer[i++] = num;
+    		}
+    	}
+    	
+        return answer;
+    }
+    
+    static int summaryUntilN(int n) {
+    	return (int) ((Math.pow(n, 2) + n)/2);
+    }
+
+}
