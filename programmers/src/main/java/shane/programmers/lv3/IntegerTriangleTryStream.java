b7754744fc6240f207d5ce63ce563df0d0527c60
Shane Park <psh40963@naver.com>
2021-08-15 10:00:10 +0900

    동일 문제 Stream으로 반환해봄

---
+++ programmers/src/main/java/shane/programmers/lv3/IntegerTriangleTryStream.java
@@ -0,0 +1,24 @@
+package shane.programmers.lv3;
+
+import java.util.Arrays;
+
+public class IntegerTriangleTryStream {
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][] { { 7 }, { 3, 8 }, { 8, 1, 0 }, { 2, 7, 4, 4 }, { 4, 5, 2, 6, 5 } }));
+
+	}
+
+	public static int solution(int[][] triangle) {
+		for (int i = 1; i < triangle.length; i++) {
+			for (int j = 0; j < triangle[i].length; j++) {
+				triangle[i][j] += j == 0 ? triangle[i - 1][0]
+						: j == triangle[i].length - 1 ? triangle[i - 1][triangle[i - 1].length - 1]
+								: Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+			}
+		}
+
+		return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
+	}
+
+}
