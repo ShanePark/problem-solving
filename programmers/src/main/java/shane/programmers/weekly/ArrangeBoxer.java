be8089cb34971dd65a700f10b2486dc91df9e7bd
Shane Park <psh40963@naver.com>
2021-09-07 21:49:21 +0900

    Comparable 구현해서 코딩해보기

--- programmers/src/main/java/shane/programmers/weekly/ArrangeBoxer.java
+++ programmers/src/main/java/shane/programmers/weekly/ArrangeBoxer.java
@@ -10,38 +10,36 @@ public class ArrangeBoxer {
         System.out.println(Arrays.toString(solution(new int[]{60,70,60}, new String[]{"NNN","NNN","NNN"})));  // 2,1,3
     }
 
-    static class Boxer{
+    static class Boxer implements Comparable<Boxer>{
         int number, weight, heavierKillingCnt;
         double winRate;
         public Boxer(int number, int weight, String history, int[] weights){
             this.number = number;
             this.weight = weight;
-            char[] arr = history.toCharArray();
             int win=0, lose=0;
-            for(int i=0; i<arr.length; i++){
-                char c = arr[i];
+            for(int i=0; i<history.length(); i++){
+                char c = history.charAt(i);
                 if(c=='W'){
                     win++;
-                    if(weights[i] > weight){
+                    if(weights[i] > weight)
                         heavierKillingCnt++;
-                    }
                 }else if(c=='L'){
                     lose++;
                 }
             }
-            if(win+lose > 0)
-                this.winRate = (win) / (double)(win+lose);
+            this.winRate = win+lose > 0 ? (double)win/(win+lose) : 0;
+        }
+        @Override
+        public int compareTo(Boxer b) {
+            return b.winRate!=winRate? (b.winRate>winRate?1:-1): b.heavierKillingCnt!=heavierKillingCnt? b.heavierKillingCnt-heavierKillingCnt: b.weight!=weight? b.weight-weight: number-b.number;
         }
     }
 
     public static int[] solution(int[] weights, String[] head2head) {
         List<Boxer> list = new ArrayList<>();
-        for(int i=0; i<weights.length; i++){
+        for(int i=0; i<weights.length; i++)
             list.add(new Boxer(i+1, weights[i], head2head[i], weights));
-        }
 
-        return list.stream()
-                .sorted((b1,b2) -> b1.winRate!=b2.winRate ? (b2.winRate>b1.winRate?1:-1) : b1.heavierKillingCnt!=b2.heavierKillingCnt ? b2.heavierKillingCnt-b1.heavierKillingCnt : b1.weight!=b2.weight? b2.weight-b1.weight : b1.number-b2.number)
-                .mapToInt(b -> b.number).toArray();
+        return list.stream().sorted().mapToInt(b -> b.number).toArray();
     }
 }
