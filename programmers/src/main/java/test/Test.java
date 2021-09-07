07d1813c404cbe5aa79b1d84c8ab9789ceedd805
Shane <psh40963@naver.com>
2021-09-07 13:39:07 +0900

    bk Tiger 질문 대답해주기

--- programmers/src/main/java/test/Test.java
+++ programmers/src/main/java/test/Test.java
@@ -1,20 +1,22 @@
 package test;
 
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-        CopyOnWriteArrayList<Integer> numbers = new CopyOnWriteArrayList<>();
-        Collections.addAll(numbers, new Integer[]{1,2,3,4,5});
-
-        Iterator<Integer> iterator = numbers.iterator();
-        while (iterator.hasNext()) {
-            Integer number = iterator.next();
-            numbers.add(6);
-            System.out.println(number);
+        String test="abcdc";
+        char[] arr = test.toCharArray();
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<arr.length; i++){
+           if(arr[i] == 'c')
+               set.add(i);
         }
+
+        Object[] result = set.toArray();
+        System.out.println(Arrays.toString(result));
+
     }
     
 }
