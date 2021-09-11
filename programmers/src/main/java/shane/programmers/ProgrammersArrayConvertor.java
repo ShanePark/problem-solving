800c3633dbb32ff9e66a19a372e3400984545fc0
Shane Park <psh40963@naver.com>
2021-09-11 19:03:13 +0900

    2022 KAKAO BLIND 4 5 6 7 실패
    후..

--- programmers/src/main/java/shane/programmers/ProgrammersArrayConvertor.java
+++ programmers/src/main/java/shane/programmers/ProgrammersArrayConvertor.java
@@ -4,7 +4,7 @@ public class ProgrammersArrayConvertor {
 
 	public static void main(String[] args) {
 		String input = 
-				"[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]";
+				"[[1,1,1,2,2,4],[1,0,0,1,1,2],[2,2,0,2,0,100]]";
 		
 		System.out.println(arrayConverter(input));
 	}
