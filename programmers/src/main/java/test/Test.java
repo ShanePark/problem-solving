4eee66348be623af3120164ea29ac91154a5f345
Shane Park <psh40963@naver.com>
2021-09-12 13:49:21 +0900

    dependency added : shaneutils

--- programmers/src/main/java/test/Test.java
+++ programmers/src/main/java/test/Test.java
@@ -1,22 +1,10 @@
 package test;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import com.tistory.shanepark.STool;
 
 public class Test {
     public static void main(String[] args) {
-        String test="abcdc";
-        char[] arr = test.toCharArray();
-        Set<Integer> set = new HashSet<>();
-        for(int i=0; i<arr.length; i++){
-           if(arr[i] == 'c')
-               set.add(i);
-        }
-
-        Object[] result = set.toArray();
-        System.out.println(Arrays.toString(result));
-
+    	System.out.println(STool.arrayConverter("[[2,4],[3,5]"));
     }
     
 }
