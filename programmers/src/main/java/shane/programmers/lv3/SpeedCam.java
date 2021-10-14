d8ddc5e72fe0dca2d6228377f7db4d4bf08339a4
Shane Park <psh40963@naver.com>
2021-10-14 09:01:42 +0900

    탐욕법 > 단속카메라 +1 point

---
+++ programmers/src/main/java/shane/programmers/lv3/SpeedCam.java
@@ -0,0 +1,62 @@
+package shane.programmers.lv3;
+
+import com.tistory.shanepark.STool;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SpeedCam {
+
+    public static void main(String[] args) {
+        System.out.println(solution(STool.convertToIntArray("[[-20,-15], [-14,-5], [-18,-13], [-5,-3]]")));
+    }
+
+    static class Car {
+        int in;
+        int out;
+
+        public Car(int in, int out) {
+            this.in = in;
+            this.out = out;
+        }
+
+        public boolean check(int spot) {
+            return (in <= spot && spot <= out) ? true : false;
+        }
+
+        @Override
+        public String toString() {
+            return "Car{" +
+                    "in=" + in +
+                    ", out=" + out +
+                    '}';
+        }
+    }
+
+    public static int solution(int[][] routes) {
+        List<Car> list = new ArrayList<>();
+        for (int[] route : routes) {
+            Car car = new Car(route[0], route[1]);
+            list.add(car);
+        }
+
+        list.sort((c1, c2) -> c1.out - c2.out);
+
+        int cnt = 0;
+
+        while(list.size() > 0) {
+            int camera = list.get(0).out;
+            cnt++;
+            List<Car> copyList = new ArrayList(list);
+            for (Car car : copyList ) {
+                if(car.check(camera)){
+                    list.remove(car);
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+}
