29346212cb0dc1118349874ba6496a683399d5dc
Shane Park <psh40963@naver.com>
2021-11-15 22:31:07 +0900

    Create Q1480RunningSumOf1dArray.java

---
+++ programmers/src/main/java/shane/leetcode/easy/Q1480RunningSumOf1dArray.java
@@ -0,0 +1,20 @@
+package shane.leetcode.easy;
+
+import com.tistory.shanepark.STool;
+
+public class Q1480RunningSumOf1dArray {
+
+    public static void main(String[] args) {
+        STool.printArray(runningSum(new int[]{1, 2, 3, 4}));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }
+
+}
