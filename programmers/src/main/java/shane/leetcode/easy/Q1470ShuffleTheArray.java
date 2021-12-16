d0a99bdea1a03d59d1f53c48db4c53c8e313147d
Shane Park <psh40963@naver.com>
2021-12-16 22:41:39 +0900

    1470. Shuffle the Array

---
+++ programmers/src/main/java/shane/leetcode/easy/Q1470ShuffleTheArray.java
@@ -0,0 +1,23 @@
+package shane.leetcode.easy;
+
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+public class Q1470ShuffleTheArray {
+
+    public static void main(String[] args) {
+        Assert.assertTrue(Arrays.equals(shuffle(new int[]{2, 5, 1, 3, 4, 7}, 3), new int[]{2, 3, 5, 4, 1, 7}));
+        Assert.assertTrue(Arrays.equals(shuffle(new int[]{1,2,3,4,4,3,2,1}, 4), new int[]{1,4,2,3,3,2,4,1}));
+    }
+
+    public static int[] shuffle(int[] nums, int n) {
+        int[] answer = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            answer[2 * i] = nums[i];
+            answer[2 * i + 1] = nums[n + i];
+        }
+        return answer;
+    }
+
+}
