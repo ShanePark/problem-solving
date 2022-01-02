f3fd6f637f00378ec56b714872d30abab1857c76
Shane Park <psh40963@naver.com>
2022-01-02 19:34:18 +0900

    leetcode 9. Palindrome Number

---
+++ programmers/src/main/java/shane/leetcode/easy/Q9PanlindromeNumber.java
@@ -0,0 +1,24 @@
+package shane.leetcode.easy;
+
+import org.junit.jupiter.api.Assertions;
+
+public class Q9PanlindromeNumber {
+    public static void main(String[] args) {
+        Assertions.assertEquals(true, isPalindrome(121));
+        Assertions.assertEquals(false, isPalindrome(-121));
+        Assertions.assertEquals(false, isPalindrome(10));
+        Assertions.assertEquals(true, isPalindrome(1001));
+    }
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        String str = String.valueOf(x);
+        int length = str.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - 1 -i))
+                return false;
+        }
+        return true;
+    }
+}
