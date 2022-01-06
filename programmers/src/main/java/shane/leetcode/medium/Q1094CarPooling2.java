f4e3a58942fe795a999a1d8d07a0df171ed94e5f
Shane Park <psh40963@naver.com>
2022-01-06 22:05:54 +0900

    Q1094 Car Pooling

---
+++ programmers/src/main/java/shane/leetcode/medium/Q1094CarPooling2.java
@@ -0,0 +1,29 @@
+package shane.leetcode.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.Assert;
+
+public class Q1094CarPooling2 {
+
+    public static void main(String[] args) {
+        Assert.assertEquals(false, carPooling(STool.convertToIntArray("[[2,1,5],[3,3,7]]"), 4));
+        Assert.assertEquals(true, carPooling(STool.convertToIntArray("[[2,1,5],[3,3,7]]"), 5));
+    }
+
+    public static boolean carPooling(int[][] trips, int capacity) {
+        int[] arr = new int[1001];
+        for (int[] trip : trips) {
+            arr[trip[1]] += trip[0];
+            arr[trip[2]] -= trip[0];
+        }
+
+        int inCar = 0;
+        for (int i = 0; i < 1001; i++) {
+            inCar += arr[i];
+            if (inCar > capacity)
+                return false;
+        }
+        return true;
+    }
+
+}
