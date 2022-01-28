a025271fdbe781d40499f42699ffe06167cb52a3
Shane Park <psh40963@naver.com>
2022-01-28 10:28:41 +0900

    Q211DesignAddandSearchWordsDataStructure
    Q421MaximumXorOfTwoNumbersInAnArray FAILED

---
+++ programmers/src/main/java/shane/leetcode/medium/Q421MaximumXorOfTwoNumbersInAnArray.java
@@ -0,0 +1,27 @@
+package shane.leetcode.medium;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Time Limit Exceeded
+ */
+public class Q421MaximumXorOfTwoNumbersInAnArray {
+
+    @Test
+    void test() {
+        Assertions.assertThat(findMaximumXOR(new int[]{3, 10, 5, 25, 2, 8})).isEqualTo(28);
+        Assertions.assertThat(findMaximumXOR(new int[]{14, 70, 53, 83, 49, 91, 36, 80, 92, 51, 66, 70})).isEqualTo(127);
+    }
+
+    public int findMaximumXOR(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                max = Math.max(max, nums[i] ^ nums[j]);
+            }
+        }
+        return max;
+    }
+
+}
