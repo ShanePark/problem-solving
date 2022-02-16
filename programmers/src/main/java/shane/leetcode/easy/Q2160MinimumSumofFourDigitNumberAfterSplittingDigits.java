c01fa0c827554fcc2b1865b18801742b34d90915
Shane Park <psh40963@naver.com>
2022-02-16 19:26:17 +0900

    Create Q2160MinimumSumofFourDigitNumberAfterSplittingDigits.java

---
+++ programmers/src/main/java/shane/leetcode/easy/Q2160MinimumSumofFourDigitNumberAfterSplittingDigits.java
@@ -0,0 +1,27 @@
+package shane.leetcode.easy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class Q2160MinimumSumofFourDigitNumberAfterSplittingDigits {
+
+    @Test
+    void test() {
+        Assertions.assertThat(minimumSum(2932)).isEqualTo(52);
+        Assertions.assertThat(minimumSum(4009)).isEqualTo(13);
+        Assertions.assertThat(minimumSum(1000)).isEqualTo(1);
+        Assertions.assertThat(minimumSum(9999)).isEqualTo(198);
+    }
+
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        arr[0] = num / 1000;
+        arr[1] = (num / 100) % 10;
+        arr[2] = (num / 10) % 10;
+        arr[3] = num % 10;
+        Arrays.sort(arr);
+        return (arr[0] + arr[1]) * 10 + arr[2] + arr[3];
+    }
+}
