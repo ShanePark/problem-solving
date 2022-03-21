5a0f7bfc0cef76ea8dfa2df3db41ac0fbba7959f
shane <psh40963@naver.com>
2022-03-21 23:11:07 +0900

    Create Q1832CheckIfTheSentenceIsPangram.java

---
+++ programmers/src/main/java/shane/leetcode/problems/easy/Q1832CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,27 @@
+package shane.leetcode.problems.easy;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q1832CheckIfTheSentenceIsPangram {
+
+    @Test
+    public void test() {
+        assertThat(checkIfPangram("thequickbrownfoxjumpsoverthelazydog")).isTrue();
+        assertThat(checkIfPangram("leetcode")).isFalse();
+    }
+
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> set = new HashSet<>();
+        int cnt = 0;
+        for (int i = 0; i < sentence.length() && cnt < 26; i++) {
+            if (set.add(sentence.charAt(i))) {
+                cnt++;
+            }
+        }
+        return cnt == 26;
+    }
+}
