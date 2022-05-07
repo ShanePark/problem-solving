e15caefc318476619b51a610c4a9ec6b1f6763b6
shane <psh40963@naver.com>
2022-05-07 13:43:43 +0900

    Create Q876MiddleOfTheLinkedList.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Middle of the Linked List.
    Memory Usage: 40 MB, less than 75.79% of Java online submissions for Middle of the Linked List.

---
+++ src/main/java/shane/leetcode/problems/easy/Q876MiddleOfTheLinkedList.java
@@ -0,0 +1,28 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q876MiddleOfTheLinkedList {
+
+    @Test
+    public void test() {
+        assertThat(middleNode(ListNode.of(1, 2, 3, 4, 5))).isEqualTo(ListNode.of(3, 4, 5));
+        assertThat(middleNode(ListNode.of(1, 2, 3, 4, 5, 6))).isEqualTo(ListNode.of(4, 5, 6));
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode normal = head;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            normal = normal.next;
+            if (fast.next != null)
+                fast = fast.next;
+        }
+        return normal;
+    }
+}
