f8bf2f7f816732e1196df32d63954f0812c7792c
shane <psh40963@naver.com>
2022-05-28 21:54:29 +0900

    Q206ReverseLinkedList

---
+++ src/main/java/shane/leetcode/problems/easy/Q206ReverseLinkedList.java
@@ -0,0 +1,38 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q206ReverseLinkedList {
+
+    @Test
+    public void test() {
+        ListNode root = ListNode.of(1, 2, 3, 4, 5);
+        assertThat(reverseList(root)).isEqualTo(ListNode.of(5, 4, 3, 2, 1));
+    }
+
+    @Test
+    public void test3() {
+        ListNode root = ListNode.of(1, 2);
+        assertThat(reverseList(root)).isEqualTo(ListNode.of(2, 1));
+    }
+
+    @Test
+    public void test2() {
+        assertThat(reverseList(null)).isNull();
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode next = head.next;
+        head.next = null;
+
+        ListNode answer = reverseList(next);
+        next.next = head;
+
+        return answer;
+    }
+}
---
+++ src/main/java/shane/leetcode/problems/easy/Q206ReverseLinkedList2.java
@@ -0,0 +1,37 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.leetcode.ListNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q206ReverseLinkedList2 {
+
+    @Test
+    public void test() {
+        ListNode root = ListNode.of(1, 2, 3, 4, 5);
+        assertThat(reverseList(root)).isEqualTo(ListNode.of(5, 4, 3, 2, 1));
+    }
+
+    @Test
+    public void test3() {
+        ListNode root = ListNode.of(1, 2);
+        assertThat(reverseList(root)).isEqualTo(ListNode.of(2, 1));
+    }
+
+    @Test
+    public void test2() {
+        assertThat(reverseList(null)).isNull();
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
