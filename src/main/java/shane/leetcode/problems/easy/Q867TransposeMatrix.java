712fe464334078ecf8c9565b70cae859cdd8aaab
shane <psh40963@naver.com>
2022-06-02 14:42:43 +0900

    Create Q867TransposeMatrix.java

---
+++ src/main/java/shane/leetcode/problems/easy/Q867TransposeMatrix.java
@@ -0,0 +1,25 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q867TransposeMatrix {
+
+    @Test
+    public void test() {
+        assertThat(transpose(STool.convertToIntArray("[[1,2,3],[4,5,6],[7,8,9]]"))).isDeepEqualTo(STool.convertToIntArray("[[1,4,7],[2,5,8],[3,6,9]]"));
+        assertThat(transpose(STool.convertToIntArray("[[1,2,3],[4,5,6]]"))).isDeepEqualTo(STool.convertToIntArray("[[1,4],[2,5],[3,6]]"));
+    }
+
+    public int[][] transpose(int[][] matrix) {
+        int[][] answer = new int[matrix[0].length][matrix.length];
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix[0].length; j++) {
+                answer[j][i] = matrix[i][j];
+            }
+        }
+        return answer;
+    }
+}
