0c1fb0d08040e9d236d06d3d98321a8a62431729
shane <psh40963@naver.com>
2022-06-13 09:31:46 +0900

    Create Q120Triangle.java
    Runtime: 8 ms, faster than 24.53% of Java online submissions for Triangle.
    Memory Usage: 44.2 MB, less than 41.06% of Java online submissions for Triangle.

---
+++ src/main/java/shane/leetcode/problems/medium/Q120Triangle.java
@@ -0,0 +1,32 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q120Triangle {
+
+    @Test
+    public void test() {
+        assertThat(minimumTotal(STool.convertToIntList("[[2],[3,4],[6,5,7],[4,1,8,3]]"))).isEqualTo(11);
+        assertThat(minimumTotal(STool.convertToIntList("[[-10]]"))).isEqualTo(-10);
+    }
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        for (int i = 1; i < triangle.size(); i++) {
+            List<Integer> before = triangle.get(i - 1);
+            List<Integer> row = triangle.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                row.set(j, Math.min(
+                        j == 0 ? Integer.MAX_VALUE : before.get(j - 1),
+                        j == before.size() ? Integer.MAX_VALUE : before.get(j)
+                ) + row.get(j));
+            }
+        }
+        return triangle.get(triangle.size() - 1).stream().min(Comparator.naturalOrder()).get();
+    }
+}
