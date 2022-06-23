11d0ae0a83188830ddf9cd0bb2893ada239042c2
shane <psh40963@naver.com>
2022-06-23 10:41:22 +0900

    630. Course Schedule III

---
+++ src/main/java/shane/leetcode/problems/xhard/Q630CourseScheduleIII.java
@@ -0,0 +1,61 @@
+package shane.leetcode.problems.xhard;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * TLE
+ */
+public class Q630CourseScheduleIII {
+
+    @Test
+    public void test() {
+//        assertThat(scheduleCourse(STool.convertToIntArray("[[914,9927],[333,712],[163,5455],[835,5040],[905,8433],[417,8249],[921,9553],[913,7394],[303,7525],[582,8658],[86,957],[40,9152],[600,6941],[466,5775],[718,8485],[34,3903],[380,9996],[316,7755]]"))).isEqualTo(0);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[100,200],[200,1300],[1000,1250],[2000,3200]]"))).isEqualTo(3);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[1,2]]"))).isEqualTo(1);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[3,2],[4,3]]"))).isEqualTo(0);
+    }
+
+    public int scheduleCourse(int[][] courses) {
+        int max = 0;
+        Queue<Status> q = new LinkedList<>();
+        int length = courses.length;
+
+        q.offer(new Status(0, 0, new boolean[length]));
+        while (!q.isEmpty()) {
+            Status status = q.poll();
+            boolean hasNext = false;
+            for (int i = 0; i < length; i++) {
+                if (!status.visited[i] && status.current <= courses[i][1] - courses[i][0]) {
+                    boolean[] visited = Arrays.copyOf(status.visited, length);
+                    visited[i] = true;
+                    q.offer(new Status(status.current + courses[i][0], status.cnt + 1, visited));
+                    hasNext = true;
+                }
+            }
+            if (!hasNext) {
+                max = Math.max(max, status.cnt);
+            }
+        }
+        return max;
+    }
+
+    class Status {
+        int current;
+        int cnt;
+        boolean[] visited;
+
+        public Status(int current, int cnt, boolean[] visited) {
+            this.current = current;
+            this.cnt = cnt;
+            this.visited = visited;
+        }
+    }
+
+}
---
+++ src/main/java/shane/leetcode/problems/xhard/Q630CourseScheduleIII2.java
@@ -0,0 +1,44 @@
+package shane.leetcode.problems.xhard;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 78 ms, faster than 21.11% of Java online submissions for Course Schedule III.
+ * Memory Usage: 74 MB, less than 30.18% of Java online submissions for Course Schedule III.
+ */
+public class Q630CourseScheduleIII2 {
+
+    @Test
+    public void test() {
+        assertThat(scheduleCourse(STool.convertToIntArray("[[100,200],[210,1300],[1000,1250],[2000,3200]]"))).isEqualTo(3);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[100,200],[200,1300],[1000,1250],[2000,3200]]"))).isEqualTo(3);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[2,5],[2,19],[1,8],[1,3]]"))).isEqualTo(4);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[1,2],[2,3]]"))).isEqualTo(2);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[914,9927],[333,712],[163,5455],[835,5040],[905,8433],[417,8249],[921,9553],[913,7394],[303,7525],[582,8658],[86,957],[40,9152],[600,6941],[466,5775],[718,8485],[34,3903],[380,9996],[316,7755]]")))
+                .isEqualTo(18);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[1,2]]"))).isEqualTo(1);
+        assertThat(scheduleCourse(STool.convertToIntArray("[[3,2],[4,3]]"))).isEqualTo(0);
+    }
+
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, Comparator.comparingInt(o -> o[1]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int current = 0;
+        for (int[] course : courses) {
+            current += course[0];
+            pq.add(course[0]);
+            if (current > course[1]) {
+                current -= pq.poll();
+            }
+        }
+        return pq.size();
+    }
+
+}
