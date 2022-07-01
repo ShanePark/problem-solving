1ee7026268dc3492abb24547fb1fa64c617a6f21
shane <psh40963@naver.com>
2022-07-01 09:07:52 +0900

    Create Q1710MaximumUnitsOnATruck3.java
    Runtime: 11 ms, faster than 69.05% of Java online submissions for Maximum Units on a Truck.
    Memory Usage: 51.2 MB, less than 33.19% of Java online submissions for Maximum Units on a Truck.

---
+++ src/main/java/shane/leetcode/problems/easy/Q1710MaximumUnitsOnATruck3.java
@@ -0,0 +1,28 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q1710MaximumUnitsOnATruck3 {
+
+    @Test
+    public void test() {
+        assertThat(maximumUnits(STool.convertToIntArray("[[1,3],[2,2],[3,1]]"), 4)).isEqualTo(8);
+        assertThat(maximumUnits(STool.convertToIntArray("[[5,10],[2,5],[4,7],[3,9]]"), 10)).isEqualTo(91);
+    }
+
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (o1, o2) -> o2[1] - o1[1]);
+        int result = 0;
+        for (int i = 0; i < boxTypes.length && truckSize > 0; i++) {
+            int min = Math.min(truckSize, boxTypes[i][0]);
+            result += min * boxTypes[i][1];
+            truckSize -= min;
+        }
+        return result;
+    }
+}
