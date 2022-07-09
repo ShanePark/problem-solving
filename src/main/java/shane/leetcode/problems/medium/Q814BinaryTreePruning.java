8921909345c7775bb32e5cf8b34b0b828e259e6a
shane <psh40963@naver.com>
2022-07-09 14:59:27 +0900

    Create Q814BinaryTreePruning.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Binary Tree Pruning.
    Memory Usage: 41.7 MB, less than 56.77% of Java online submissions for Binary Tree Pruning.

---
+++ src/main/java/shane/leetcode/problems/medium/Q814BinaryTreePruning.java
@@ -0,0 +1,44 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q814BinaryTreePruning {
+
+    @Test
+    public void test() {
+        TreeNode start = TreeNode.of(1, null, 0, null, null, 0, 1);
+        TreeNode end = pruneTree(start);
+        assertThat(end).isEqualTo(TreeNode.of(1, null, 0, null, null, null, 1));
+    }
+
+    @Test
+    public void test2() {
+        TreeNode start = TreeNode.of(0, null, 0, null, null, 0, 0);
+        TreeNode end = pruneTree(start);
+        assertThat(end).isNull();
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        return hasOne(root) ? root : null;
+    }
+
+    private boolean hasOne(TreeNode root) {
+        if (root == null)
+            return false;
+
+        boolean left = hasOne(root.left);
+        if (!left) {
+            root.left = null;
+        }
+
+        boolean right = hasOne(root.right);
+        if (!right) {
+            root.right = null;
+        }
+
+        return root.val == 1 || left || right;
+    }
+}
