f25cde29dd7fd6fa0a63f8eb80a14c46fb72c25a
shane <psh40963@naver.com>
2022-07-10 21:49:53 +0900

    Create Q2331EvaluateBooleanBinaryTree.java
    Runtime: 0 ms, faster than 100.00% of Java online submissions for Evaluate Boolean Binary Tree.
    Memory Usage: 42.2 MB, less than 100.00% of Java online submissions for Evaluate Boolean Binary Tree.

---
+++ src/main/java/shane/leetcode/problems/easy/Q2331EvaluateBooleanBinaryTree.java
@@ -0,0 +1,28 @@
+package shane.leetcode.problems.easy;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q2331EvaluateBooleanBinaryTree {
+
+    @Test
+    public void test() {
+        assertThat(evaluateTree(TreeNode.of(2, 1, 3, null, null, 0, 1))).isTrue();
+    }
+
+    public boolean evaluateTree(TreeNode root) {
+        switch (root.val) {
+            case 1:
+                return true;
+            case 2:
+                return evaluateTree(root.left) || evaluateTree(root.right);
+            case 3:
+                return evaluateTree(root.left) && evaluateTree(root.right);
+            default:
+                return false;
+        }
+    }
+
+}
