5385067dc8c248ee8fc04bf838c3a969fd1e9bd3
shane <psh40963@naver.com>
2022-07-11 09:11:35 +0900

    Create Q199BinaryTreeRightSideView.java
    Runtime: 13 ms, faster than 11.45% of Java online submissions for Binary Tree Right Side View.
    Memory Usage: 43.3 MB, less than 5.70% of Java online submissions for Binary Tree Right Side View.

---
+++ src/main/java/shane/leetcode/problems/medium/Q199BinaryTreeRightSideView.java
@@ -0,0 +1,43 @@
+package shane.leetcode.problems.medium;
+
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Q199BinaryTreeRightSideView {
+
+    @Test
+    public void test() {
+        assertThat(rightSideView(TreeNode.of(1, 2, 3, null, 5, null, 4))).containsExactly(1, 3, 4);
+        assertThat(rightSideView(TreeNode.of(1, 2))).containsExactly(1, 2);
+        assertThat(rightSideView(TreeNode.of())).containsExactly();
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        dfs(root, map, 0);
+        List<Integer> answer = new ArrayList<>();
+        map.keySet().stream().sorted().forEach(depth -> {
+            List<Integer> list = map.get(depth);
+            answer.add(list.get(list.size() - 1));
+        });
+        return answer;
+    }
+
+    private void dfs(TreeNode root, Map<Integer, List<Integer>> map, int depth) {
+        if (root == null)
+            return;
+        List<Integer> list = map.getOrDefault(depth, new ArrayList<>());
+        list.add(root.val);
+        map.put(depth, list);
+        dfs(root.left, map, depth + 1);
+        dfs(root.right, map, depth + 1);
+    }
+}
