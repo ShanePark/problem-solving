d2ee811ff40293e4da211e2fecfe6836026e0f82
shane <psh40963@naver.com>
2022-07-12 22:58:18 +0900

    1123. Lowest Common Ancestor of Deepest Leaves
    Runtime: 1 ms, faster than 84.03% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.
    Memory Usage: 44.2 MB, less than 71.67% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.

---
+++ src/main/java/shane/leetcode/problems/medium/Q1123LowestCommonAncestorOfDeepestLeaves.java
@@ -0,0 +1,63 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 8 ms, faster than 5.28% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.
+ * Memory Usage: 44.8 MB, less than 39.50% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.
+ */
+public class Q1123LowestCommonAncestorOfDeepestLeaves {
+
+    @Test
+    public void test() {
+        assertThat(lcaDeepestLeaves(TreeNode.of(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4))).isEqualTo(TreeNode.of(2, 7, 4));
+        assertThat(lcaDeepestLeaves(TreeNode.of(1))).isEqualTo(TreeNode.of(1));
+        assertThat(lcaDeepestLeaves(TreeNode.of(0, 1, 3, null, 2))).isEqualTo(TreeNode.of(2));
+    }
+
+
+    int maxDepth;
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        maxDepth = 0;
+        Set<TreeNode> deepest = new HashSet<>();
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        dfs(root, 0, deepest, parentMap);
+
+        while (deepest.size() > 1) {
+            Set<TreeNode> parents = new HashSet<>();
+            for (TreeNode node : deepest) {
+                parents.add(parentMap.get(node));
+            }
+            deepest = parents;
+        }
+
+        return deepest.stream().findAny().get();
+    }
+
+    private void dfs(TreeNode root, int depth, Set<TreeNode> deepest, Map<TreeNode, TreeNode> parentMap) {
+        if (maxDepth == depth) {
+            deepest.add(root);
+        } else if (maxDepth < depth) {
+            maxDepth = depth;
+            deepest.clear();
+            deepest.add(root);
+        }
+        if (root.left != null) {
+            parentMap.put(root.left, root);
+            dfs(root.left, depth + 1, deepest, parentMap);
+        }
+        if (root.right != null) {
+            parentMap.put(root.right, root);
+            dfs(root.right, depth + 1, deepest, parentMap);
+        }
+    }
+}
---
+++ src/main/java/shane/leetcode/problems/medium/Q1123LowestCommonAncestorOfDeepestLeaves2.java
@@ -0,0 +1,38 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 1 ms, faster than 84.03% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.
+ * Memory Usage: 44.2 MB, less than 71.67% of Java online submissions for Lowest Common Ancestor of Deepest Leaves.
+ */
+public class Q1123LowestCommonAncestorOfDeepestLeaves2 {
+
+    @Test
+    public void test() {
+        assertThat(lcaDeepestLeaves(TreeNode.of(3, 5, 1, 6, 2, 0, 8, null, null, 7, 4))).isEqualTo(TreeNode.of(2, 7, 4));
+        assertThat(lcaDeepestLeaves(TreeNode.of(1))).isEqualTo(TreeNode.of(1));
+        assertThat(lcaDeepestLeaves(TreeNode.of(0, 1, 3, null, 2))).isEqualTo(TreeNode.of(2));
+    }
+
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        if (leftDepth == rightDepth) {
+            return root;
+        } else {
+            return (leftDepth > rightDepth) ? lcaDeepestLeaves(root.left) : lcaDeepestLeaves(root.right);
+        }
+    }
+
+    private int maxDepth(TreeNode node) {
+        if (node == null)
+            return 0;
+        return Math.max(maxDepth(node.left), maxDepth(node.right)) + 1;
+    }
+
+}
