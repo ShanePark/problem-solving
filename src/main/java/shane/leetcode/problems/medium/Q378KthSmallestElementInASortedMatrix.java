5a6b7227250cb6e74998fb023eb2eafe88ea29e0
shane <psh40963@naver.com>
2022-08-02 18:03:51 +0900

    Q378KthSmallestElementInASortedMatrix

---
+++ src/main/java/shane/leetcode/problems/medium/Q378KthSmallestElementInASortedMatrix.java
@@ -0,0 +1,33 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 42 ms, faster than 14.29% of Java online submissions for Kth Smallest Element in a Sorted Matrix.
+ * Memory Usage: 55.2 MB, less than 83.46% of Java
+ */
+public class Q378KthSmallestElementInASortedMatrix {
+
+    @Test
+    public void test() {
+        assertThat(kthSmallest(STool.convertToIntArray("[[1,5,9],[10,11,13],[12,13,15]]"), 8)).isEqualTo(13);
+        assertThat(kthSmallest(STool.convertToIntArray("[[-5]]"), 1)).isEqualTo(-5);
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+        List<Integer> list = new ArrayList<>();
+        for (int[] ints : matrix) {
+            for (int i : ints) {
+                list.add(i);
+            }
+        }
+        return list.stream().sorted().collect(Collectors.toList()).get(k - 1);
+    }
+}
---
+++ src/main/java/shane/leetcode/problems/medium/Q378KthSmallestElementInASortedMatrix2.java
@@ -0,0 +1,46 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 1 ms, faster than 92.44% of Java online submissions for Kth Smallest Element in a Sorted Matrix.
+ * Memory Usage: 57.1 MB, less than 39.62% of Java online submissions for Kth Smallest Element in a Sorted Matrix.
+ */
+public class Q378KthSmallestElementInASortedMatrix2 {
+
+    @Test
+    public void test() {
+        assertThat(kthSmallest(STool.convertToIntArray("[[-5,-4],[-5,-4]]"), 2)).isEqualTo(-5);
+        assertThat(kthSmallest(STool.convertToIntArray("[[1,5,9],[10,11,13],[12,13,15]]"), 8)).isEqualTo(13);
+        assertThat(kthSmallest(STool.convertToIntArray("[[-5]]"), 1)).isEqualTo(-5);
+    }
+
+    public int kthSmallest(int[][] matrix, int k) {
+        final int WIDTH = matrix[0].length;
+        final int HEIGHT = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[HEIGHT - 1][WIDTH - 1];
+
+        while (left < right) {
+            int mid = (left + right - 1) / 2;
+            int cnt = 0;
+            int j = WIDTH - 1;
+            for (int i = 0; i < HEIGHT; i++) {
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                cnt += (j + 1);
+            }
+            
+            if (cnt < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
