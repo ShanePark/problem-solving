bc9df96869dbbe08279dae5fbd53a28ec9285d53
shane <psh40963@naver.com>
2022-08-13 21:47:57 +0900

    Create Q841KeysAndRooms.java

---
+++ src/main/java/shane/leetcode/problems/medium/Q841KeysAndRooms.java
@@ -0,0 +1,39 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 3 ms, faster than 56.42% of Java online submissions for Keys and Rooms.
+ * Memory Usage: 45.9 MB, less than 5.51% of Java online submissions for Keys and Rooms.
+ */
+public class Q841KeysAndRooms {
+
+    @Test
+    public void test() {
+        assertThat(canVisitAllRooms(STool.convertToIntList("[[1],[2],[3],[]]"))).isEqualTo(true);
+        assertThat(canVisitAllRooms(STool.convertToIntList("[[1,3],[3,0,1],[2],[0]]"))).isEqualTo(false);
+    }
+
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        set.addAll(rooms.get(0));
+        Queue<Integer> q = new LinkedList<>();
+        q.addAll(rooms.get(0));
+        while (!q.isEmpty()) {
+            Integer poll = q.poll();
+            List<Integer> keys = rooms.get(poll);
+            for (Integer key : keys) {
+                if (set.add(key)) {
+                    q.add(key);
+                }
+            }
+        }
+        return set.size() == rooms.size();
+    }
+}
