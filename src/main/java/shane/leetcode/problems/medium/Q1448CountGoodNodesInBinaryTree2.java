106cd4053375f300ab928d0052d513931366d83c
shane <psh40963@naver.com>
2022-09-01 09:05:58 +0900

    Create Q1448CountGoodNodesInBinaryTree2.java

---
+++ src/main/java/shane/leetcode/problems/medium/Q1448CountGoodNodesInBinaryTree2.java
@@ -0,0 +1,37 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime: 3 ms, faster than 76.91% of Java online submissions for Count Good Nodes in Binary Tree.
+ * Memory Usage: 60.3 MB, less than 20.48% of Java online submissions for Count Good Nodes in Binary Tree.
+ */
+public class Q1448CountGoodNodesInBinaryTree2 {
+    @Test
+    public void test() {
+        assertThat(goodNodes(TreeNode.of(3, 1, 4, 3, null, 1, 5))).isEqualTo(4);
+        assertThat(goodNodes(TreeNode.of(3, 3, null, 4, 2))).isEqualTo(3);
+        assertThat(goodNodes(TreeNode.of(1))).isEqualTo(1);
+    }
+
+    int cnt;
+
+    public int goodNodes(TreeNode root) {
+        cnt = 0;
+        dfs(root, root.val);
+        return cnt;
+    }
+
+    private void dfs(TreeNode node, int val) {
+        if (node == null)
+            return;
+        int cur = node.val;
+        if (cur >= val)
+            cnt++;
+        dfs(node.left, Math.max(val, cur));
+        dfs(node.right, Math.max(val, cur));
+    }
+}
