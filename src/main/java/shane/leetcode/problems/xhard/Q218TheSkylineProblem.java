a17ec82648f5d7d152618d5a6ec603d218011e08
shane <psh40963@naver.com>
2022-09-30 12:43:28 +0900

    Q218TheSkylineProblem

---
+++ src/main/java/shane/leetcode/problems/xhard/Q218TheSkylineProblem.java
@@ -0,0 +1,60 @@
+package shane.leetcode.problems.xhard;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * TLE
+ */
+public class Q218TheSkylineProblem {
+
+    @Test
+    public void test() {
+        assertThat(getSkyline(STool.convertToIntArray("[[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]")))
+                .isEqualTo(STool.convertToIntList("[[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]"));
+        assertThat(getSkyline(STool.convertToIntArray("[[0,2,3],[2,5,3]]")))
+                .isEqualTo(STool.convertToIntList("[[0,3],[5,0]]"));
+    }
+
+    @Test
+    public void tle() {
+//        assertThat(getSkyline(STool.convertToIntArray("[[0,2147483647,2147483647]]")));
+    }
+
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        Map<Integer, Integer> heightMap = new LinkedHashMap<>();
+        int last = -1;
+        for (int[] building : buildings) {
+            int left = building[0];
+            int right = building[1];
+            last = Math.max(last, right);
+            int height = building[2];
+            for (; left < right; left++) {
+                Integer beforeMax = heightMap.getOrDefault(left, 0);
+                heightMap.put(left, Math.max(beforeMax, height));
+            }
+        }
+
+        int heightBefore = -1;
+        List<List<Integer>> answer = new ArrayList<>();
+        for (int i = buildings[0][0]; i < last; i++) {
+            Integer height = heightMap.getOrDefault(i, 0);
+            if (heightBefore != height) {
+                answer.add(List.of(i, height));
+            }
+            heightBefore = height;
+        }
+
+        answer.add(List.of(last, 0));
+
+        return answer;
+    }
+
+}
---
+++ src/main/java/shane/leetcode/problems/xhard/Q218TheSkylineProblem2.java
@@ -0,0 +1,93 @@
+package shane.leetcode.problems.xhard;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Got Heap idea from the community
+ *
+ * Runtime: 466 ms, faster than 14.28% of Java online submissions for The Skyline Problem.
+ * Memory Usage: 51.9 MB, less than 62.94% of Java online submissions for The Skyline Problem.
+ */
+public class Q218TheSkylineProblem2 {
+
+    @Test
+    public void test() {
+        assertThat(getSkyline(STool.convertToIntArray("[[1,2,1],[1,2,2],[1,2,3]]"))).isEqualTo(STool.convertToIntList("[[1,3],[2,0]]"));
+        assertThat(getSkyline(STool.convertToIntArray("[[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]")))
+                .isEqualTo(STool.convertToIntList("[[2,10],[3,15],[7,12],[12,0],[15,10],[20,8],[24,0]]"));
+        assertThat(getSkyline(STool.convertToIntArray("[[0,2,3],[2,5,3]]")))
+                .isEqualTo(STool.convertToIntList("[[0,3],[5,0]]"));
+    }
+
+    @Test
+    public void tle() {
+        assertThat(getSkyline(STool.convertToIntArray("[[0,2147483647,2147483647]]"))).isEqualTo(STool.convertToIntList("[[0, 2147483647], [2147483647, 0]]"));
+    }
+
+    public List<List<Integer>> getSkyline(int[][] buildings) {
+        List<List<Integer>> answer = new ArrayList<>();
+        List<Change> changes = new ArrayList<>();
+        for (int[] b : buildings) {
+            int left = b[0];
+            int right = b[1];
+            int height = b[2];
+            changes.add(new Change(left, Status.START, height));
+            changes.add(new Change(right, Status.END, height));
+        }
+
+        Collections.sort(changes);
+
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        pq.offer(0);
+        int beforeHeight = -1;
+        for (Change h : changes) {
+            if (h.status == Status.START) {
+                pq.offer(h.height);
+            } else {
+                pq.remove(h.height);
+            }
+            int currentHeight = pq.peek();
+            if (beforeHeight != currentHeight) {
+                answer.add(List.of(h.place, currentHeight));
+                beforeHeight = currentHeight;
+            }
+        }
+        return answer;
+    }
+
+    private class Change implements Comparable {
+        int place;
+        Status status;
+        int height;
+
+        public Change(int place, Status status, int height) {
+            this.place = place;
+            this.status = status;
+            this.height = height;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            Change b = (Change) o;
+            if (place != b.place)
+                return place - b.place;
+            if (status != b.status) {
+                return status == Status.START ? -1 : 1;
+            }
+            if (status == Status.START) {
+                return b.height - height;
+            } else {
+                return height - b.height;
+            }
+        }
+    }
+
+    enum Status {
+        START, END
+    }
+}
