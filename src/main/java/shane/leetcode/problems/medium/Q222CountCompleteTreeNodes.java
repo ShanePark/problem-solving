21e00b8316da338cf6aea987732c57617c5a4454
shane <psh40963@naver.com>
2022-11-15 09:05:38 +0900

    Q222CountCompleteTreeNodes

---
+++ src/main/java/shane/leetcode/problems/medium/Q222CountCompleteTreeNodes.java
@@ -0,0 +1,41 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 0 ms Beats 100%
+ * Memory 50 MB Beats 41.59%
+ */
+public class Q222CountCompleteTreeNodes {
+
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        assertThat(countNodes(root)).isEqualTo(6);
+    }
+
+    @Test
+    public void test2() {
+        TreeNode root = new TreeNode(1);
+        assertThat(countNodes(root)).isEqualTo(1);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(countNodes(null)).isEqualTo(0);
+    }
+
+    public int countNodes(TreeNode root) {
+        if (root == null)
+            return 0;
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
+}
