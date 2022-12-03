ce5fadf16dd2d43fc1ff1d7bd426043c4ec53285
shane <psh40963@naver.com>
2022-12-03 15:55:06 +0900

    Create Q2482DifferenceBetweenOnesAndZerosInRowAndColumn.java

---
+++ src/main/java/shane/leetcode/problems/medium/Q2482DifferenceBetweenOnesAndZerosInRowAndColumn.java
@@ -0,0 +1,44 @@
+package shane.leetcode.problems.medium;
+
+import com.tistory.shanepark.STool;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 48 ms Beats 18.45%
+ * Memory 145.4 MB Beats 71.79%
+ */
+public class Q2482DifferenceBetweenOnesAndZerosInRowAndColumn {
+
+    @Test
+    public void test() {
+        assertThat(onesMinusZeros(STool.convertToIntArray("[[0,1,1],[1,0,1],[0,0,1]]"))).isDeepEqualTo(STool.convertToIntArray("[[0,0,4],[0,0,4],[-2,-2,2]]"));
+        assertThat(onesMinusZeros(STool.convertToIntArray("[[1,1,1],[1,1,1]]"))).isDeepEqualTo(STool.convertToIntArray("[[5,5,5],[5,5,5]]"));
+
+    }
+
+    public int[][] onesMinusZeros(int[][] grid) {
+        int height = grid.length;
+        int width = grid[0].length;
+        int[][] answer = new int[height][width];
+        int[] rows = new int[height];
+        int[] cols = new int[width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                rows[i] += grid[i][j] == 1 ? 1 : -1;
+                cols[j] += grid[i][j] == 1 ? 1 : -1;
+            }
+        }
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                answer[i][j] += cols[j] + rows[i];
+            }
+        }
+
+        return answer;
+    }
+
+}
