a4cd33a09afa05e0062a127357d2e780db1b3edf
shane <psh40963@naver.com>
2022-12-30 09:36:04 +0900

    Create Q797AllPathsFromSourceToTargetK.kt

---
+++ src/main/kotlin/shane/leetcode/problems/medium/Q797AllPathsFromSourceToTargetK.kt
@@ -0,0 +1,79 @@
+package shane.leetcode.problems.medium
+
+import com.tistory.shanepark.STool
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+/**
+ * Runtime 295 ms Beats 96.36%
+ * Memory 39.2 MB Beats 100%
+ */
+class Q797AllPathsFromSourceToTargetK {
+
+    @Test
+    fun test() {
+        Assertions.assertThat(allPathsSourceTarget(STool.convertToIntArray("[[1,2],[3],[3],[]]")))
+            .containsAll(STool.convertToIntList("[[0,1,3],[0,2,3]]"))
+        Assertions.assertThat(allPathsSourceTarget(STool.convertToIntArray("[[4,3,1],[3,2,4],[3],[4],[]]")))
+            .containsAll(STool.convertToIntList("[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]"))
+    }
+
+    fun allPathsSourceTarget(graph: Array<IntArray>): List<List<Int>> {
+        val map = mutableMapOf<Int, Node>()
+        for (i in graph.indices) {
+            for (j in graph[i]) {
+                val node = map.getOrDefault(i, Node(i))
+                val next = map.getOrDefault(j, Node(j))
+
+                node.addNext(next)
+
+                map[i] = node
+                map[j] = next
+            }
+        }
+
+        val visited = BooleanArray(graph.size)
+        val answer = mutableListOf<List<Int>>()
+        visited[0] = true
+
+        dfs(map[0]!!, map.size - 1, mutableListOf(0), visited, answer)
+        return answer
+    }
+
+    private fun dfs(
+        node: Node,
+        target: Int,
+        list: MutableList<Int>,
+        visited: BooleanArray,
+        answer: MutableList<List<Int>>
+    ) {
+        if (node.index == target) {
+            answer.add(list.toMutableList())
+            return
+        }
+
+        for (next in node.next) {
+            if (!visited[next.index]) {
+                visited[next.index] = true
+                list.add(next.index)
+
+                dfs(next, target, list, visited, answer)
+
+                visited[next.index] = false
+                list.remove(next.index)
+            }
+        }
+
+    }
+
+    class Node(
+        val index: Int
+    ) {
+        val next = mutableListOf<Node>()
+
+        fun addNext(n: Node) {
+            next.add(n)
+        }
+    }
+
+}
