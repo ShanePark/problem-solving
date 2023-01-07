bae3e10777e55028c8742addce20051e352e2bc8
shane <psh40963@naver.com>
2023-01-07 09:11:41 +0900

    Q134GasStation

--- src/main/java/shane/leetcode/problems/medium/Q134GasStation3.java
+++ src/main/java/shane/leetcode/problems/medium/Q134GasStation3.java
@@ -1,29 +1,29 @@
 package shane.leetcode.problems.medium;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class Q134GasStation2 {
+/**
+ * Runtime 1 ms Beats 100%
+ * Memory 62.6 MB Beats 72.25%
+ */
+public class Q134GasStation3 {
 
-    public static void main(String[] args) {
+    @Test
+    public void test() {
+        Assertions.assertThat(canCompleteCircuit(new int[]{3, 1, 1}, new int[]{1, 2, 2})).isEqualTo(0);
         Assertions.assertThat(canCompleteCircuit(new int[]{1, 2, 3, 4, 5}, new int[]{3, 4, 5, 1, 2})).isEqualTo(3);
         Assertions.assertThat(canCompleteCircuit(new int[]{2, 3, 4}, new int[]{3, 4, 3})).isEqualTo(-1);
         Assertions.assertThat(canCompleteCircuit(new int[]{5, 1, 2, 3, 4}, new int[]{4, 4, 1, 5, 1})).isEqualTo(4);
-        Assertions.assertThat(canCompleteCircuit(new int[]{3, 1, 1}, new int[]{1, 2, 2})).isEqualTo(0);
     }
 
-    public static int canCompleteCircuit(int[] gas, int[] cost) {
-        int temp = 0;
+    public int canCompleteCircuit(int[] gas, int[] cost) {
         for (int i = 0; i < gas.length; i++) {
             gas[i] -= cost[i];
-            temp += gas[i];
         }
-
-        if (temp < 0)
-            return -1;
-
+        int sum = 0;
         int min = Integer.MAX_VALUE;
         int minIndex = 0;
-        int sum = 0;
         for (int i = 0; i < gas.length; i++) {
             sum += gas[i];
             if (sum < min) {
@@ -31,9 +31,9 @@ public class Q134GasStation2 {
                 minIndex = i;
             }
         }
-
+        if (sum < 0)
+            return -1;
         return (minIndex + 1) % gas.length;
-
     }
 
 }
