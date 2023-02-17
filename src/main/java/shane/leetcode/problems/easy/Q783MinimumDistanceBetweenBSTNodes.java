863a82e8de6bdb5fc70ccf6ef758d13b1278f267
shane <psh40963@naver.com>
2023-02-17 09:09:28 +0900

    Create Q783MinimumDistanceBetweenBSTNodes.java

---
+++ src/main/java/shane/leetcode/problems/easy/Q783MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,41 @@
+package shane.leetcode.problems.easy;
+
+import com.github.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 0 ms Beats 100%
+ * Memory 39.6 MB Beats 92.86%
+ */
+public class Q783MinimumDistanceBetweenBSTNodes {
+
+    @Test
+    public void test() {
+        assertThat(minDiffInBST(TreeNode.of(4, 2, 6, 1, 3))).isEqualTo(1);
+        assertThat(minDiffInBST(TreeNode.of(1, 0, 48, null, null, 12, 49))).isEqualTo(1);
+    }
+
+    public int minDiffInBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < list.size() - 1; i++) {
+            minDiff = Math.min(minDiff, list.get(i + 1) - list.get(i));
+        }
+        return minDiff;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> list) {
+        if (root == null)
+            return;
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+
+}
