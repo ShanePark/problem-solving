7142a2863e3a0fb2adb47528549afd5c76542462
shane <psh40963@naver.com>
2023-02-18 09:03:29 +0900

    Create Q226InvertBinaryTree.java

---
+++ src/main/java/shane/leetcode/Q226InvertBinaryTree.java
@@ -0,0 +1,30 @@
+package shane.leetcode;
+
+import com.github.shanepark.leetcode.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 0 ms Beats 100%
+ * Memory 40 MB Beats 48.41%
+ */
+public class Q226InvertBinaryTree {
+
+    @Test
+    void test() {
+        assertThat(invertTree(TreeNode.of(4, 2, 7, 1, 3, 6, 9)))
+                .isEqualTo(TreeNode.of(4, 7, 2, 9, 6, 3, 1));
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+
+}
