b235705d4faf5bd307c9c70af81f6b935ec85669
shane <shanepark.dev@gmail.com>
2023-08-10 09:24:03 +0900

    Create Q81SearchInRotatedSortedArrayII2.java

---
+++ src/main/java/shane/leetcode/problems/medium/Q81SearchInRotatedSortedArrayII2.java
@@ -0,0 +1,30 @@
+package shane.leetcode.problems.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 2 ms Beats 7.14% Memory 43.7 MB Beats 7.65%
+ */
+public class Q81SearchInRotatedSortedArrayII2 {
+
+    @Test
+    public void test() {
+        assertThat(search(new int[]{1, 0, 1, 1, 1}, 0)).isTrue();
+        assertThat(search(new int[]{2, 5, 0, 0, 1, 2}, 5)).isTrue();
+        assertThat(search(new int[]{2, 5, 6, 0, 0, 1, 2}, 0)).isTrue();
+        assertThat(search(new int[]{2, 5, 6, 0, 0, 1, 2}, 3)).isFalse();
+        assertThat(search(new int[]{2, 5, 0, 0, 1, 2}, 3)).isFalse();
+        assertThat(search(new int[]{2}, 3)).isFalse();
+        assertThat(search(new int[]{3}, 3)).isTrue();
+    }
+
+    public boolean search(int[] nums, int target) {
+        Arrays.sort(nums);
+        return Arrays.binarySearch(nums, target) >= 0;
+    }
+
+}
