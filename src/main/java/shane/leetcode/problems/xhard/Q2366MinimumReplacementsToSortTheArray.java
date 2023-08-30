a26a3e8f6b9d962925665784d7483fc373e8ebb7
shane <shanepark.dev@gmail.com>
2023-08-30 12:55:49 +0900

    Update Q2366MinimumReplacementsToSortTheArray.java

--- src/main/java/shane/leetcode/problems/xhard/Q2366MinimumReplacementsToSortTheArray.java
+++ src/main/java/shane/leetcode/problems/xhard/Q2366MinimumReplacementsToSortTheArray.java
@@ -8,22 +8,24 @@ public class Q2366MinimumReplacementsToSortTheArray {
 
     @Test
     public void test() {
-        assertThat(minimumReplacement(new int[]{3, 9, 3})).isEqualTo(2);
         assertThat(minimumReplacement(new int[]{3, 10, 3})).isEqualTo(4);
+        assertThat(minimumReplacement(new int[]{3, 9, 3})).isEqualTo(2);
         assertThat(minimumReplacement(new int[]{1, 2, 3, 4, 5})).isEqualTo(0);
+        assertThat(minimumReplacement(new int[]{19, 7, 2, 24, 11, 16, 1, 11, 23})).isEqualTo(73);
     }
 
     public long minimumReplacement(int[] nums) {
         long answer = 0;
-        for (int i = nums.length - 2; i >= 0; i--) {
-            int cur = nums[i];
-            int next = nums[i + 1];
-            if (cur <= next) {
+        int min = nums[nums.length - 1];
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] <= min) {
+                min = nums[i];
                 continue;
             }
-            int cnt = (cur + next - 1) / next;
-            nums[i] = nums[i] / cnt;
 
+            int cnt = nums[i] / min + (nums[i] % min == 0 ? 0 : 1);
+            min = nums[i] / cnt;
             answer += (cnt - 1);
         }
 
