1ff7b4e53e9386634059578a15e405fed691a1d5
shane <psh40963@naver.com>
2024-02-04 20:10:31 +0900

    Create Q76MinimumWindowSubstring2.java

---
+++ src/main/java/shane/leetcode/problems/xhard/Q76MinimumWindowSubstring2.java
@@ -0,0 +1,63 @@
+package shane.leetcode.problems.xhard;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 236 ms Beats 5.89% of users with Java
+ */
+public class Q76MinimumWindowSubstring2 {
+
+    @Test
+    public void test() {
+        assertThat(minWindow("cabwefgewcwaefgcf", "cae")).isEqualTo("cwae");
+        assertThat(minWindow("ADOBECODEBANC", "ABC")).isEqualTo("BANC");
+        assertThat(minWindow("a", "a")).isEqualTo("a");
+        assertThat(minWindow("a", "aa")).isEqualTo("");
+        assertThat(minWindow("ABCD", "ACD")).isEqualTo("ABCD");
+        assertThat(minWindow("ABCD", "ACD")).isEqualTo("ABCD");
+        assertThat(minWindow("AAAA", "AA")).isEqualTo("AA");
+    }
+
+    public String minWindow(String s, String t) {
+        int left = 0;
+        int right = 0;
+        String answer = "";
+        Map<Character, Integer> cnt = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            cnt.merge(c, -1, Integer::sum);
+        }
+
+        StringBuilder cur = new StringBuilder();
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            cur.append(c);
+            int val = cnt.merge(c, 1, Integer::sum);
+            if (val == 0 && isValid(cnt)) {
+                // When it's valid, move left
+                while (isValid(cnt)) {
+                    if (answer.isEmpty() || cur.length() < answer.length()) {
+                        answer = cur.toString();
+                    }
+                    cur.deleteCharAt(0);
+                    cnt.merge(s.charAt(left++), -1, Integer::sum);
+                }
+            }
+            right++;
+        }
+
+        return answer;
+    }
+
+    private boolean isValid(Map<Character, Integer> cnt) {
+        return cnt.values().stream()
+                .filter(v -> v < 0)
+                .findAny()
+                .isEmpty();
+    }
+
+}
