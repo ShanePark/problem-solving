290315c1393b92d2a07259019561673f2af20a00
shane <shanepark.dev@gmail.com>
2024-02-05 09:15:24 +0900

    Create Q387FirstUniqueCharacterInAString2.java

---
+++ src/main/java/shane/leetcode/problems/easy/Q387FirstUniqueCharacterInAString2.java
@@ -0,0 +1,35 @@
+package shane.leetcode.problems.easy;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime 22 ms Beats 56.63% of users with Java
+ */
+public class Q387FirstUniqueCharacterInAString2 {
+
+    @Test
+    public void test() {
+        assertThat(firstUniqChar("leetcode")).isEqualTo(0);
+        assertThat(firstUniqChar("loveleetcode")).isEqualTo(2);
+        assertThat(firstUniqChar("aabb")).isEqualTo(-1);
+    }
+
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> cnt = new LinkedHashMap<>();
+        for (char c : s.toCharArray()) {
+            cnt.merge(c, 1, Integer::sum);
+        }
+        for (Map.Entry<Character, Integer> e : cnt.entrySet()) {
+            if (e.getValue() == 1) {
+                return s.indexOf(e.getKey());
+            }
+        }
+        return -1;
+    }
+
+}
