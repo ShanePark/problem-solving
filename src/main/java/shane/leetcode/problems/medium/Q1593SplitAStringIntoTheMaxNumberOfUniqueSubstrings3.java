5cbcc341ad9207e1d361161c93a5aa2a0ca5a442
shane <shanepark.dev@gmail.com>
2024-10-21 09:23:48 +0900

    Create Q1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings3.java

---
+++ src/main/java/shane/leetcode/problems/medium/Q1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings3.java
@@ -0,0 +1,46 @@
+package shane.leetcode.problems.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Runtime52msBeats23.86%
+ */
+public class Q1593SplitAStringIntoTheMaxNumberOfUniqueSubstrings3 {
+
+    @Test
+    public void test() {
+        assertThat(maxUniqueSplit("wwwzfvedwfvhsww")).isEqualTo(11);
+        assertThat(maxUniqueSplit("ababccc")).isEqualTo(5);
+        assertThat(maxUniqueSplit("aba")).isEqualTo(2);
+        assertThat(maxUniqueSplit("aa")).isEqualTo(1);
+    }
+
+    int max = 0;
+
+    public int maxUniqueSplit(String s) {
+        max = 0;
+        dfs(s, 0, new StringBuilder(), new HashSet<>());
+        return max;
+    }
+
+    private void dfs(String s, int i, StringBuilder cur, Set<String> set) {
+        if (s.length() == i) {
+            max = Math.max(max, set.size());
+            return;
+        }
+        char c = s.charAt(i);
+        cur.append(c);
+        dfs(s, i + 1, cur, set);
+        if (set.add(cur.toString())) {
+            dfs(s, i + 1, new StringBuilder(), set);
+            set.remove(cur.toString());
+        }
+        cur.deleteCharAt(cur.length() - 1);
+    }
+
+}
